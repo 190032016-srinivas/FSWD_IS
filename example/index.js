a74d5dfcd82f6ef3fb37cf01e31d655006ed9269
190032016-srinivas <74003608+190032016-srinivas@users.noreply.github.com>
2024-07-03 18:17:15 +0530

    test

---
+++ example/index.js
@@ -0,0 +1,7 @@
+console.log('gi')
+const hi = document.getElementById('lol')
+let newD = document.getElementById('newD')
+console.log('newD',newD)
+hi.style.color = 'blue'
+hi.style.backgroundColor = 'red'
+console.log(hi.style.color)
