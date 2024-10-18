730c4681dfe996e707eec1aba2230eb3a2fce1fb
190032016-srinivas <74003608+190032016-srinivas@users.noreply.github.com>
2024-10-18 22:14:02 +0530

    removed all unnecessary files (node_modules)

--- example/index.js
+++
@@ -1,7 +0,0 @@
-console.log('gi')
-const hi = document.getElementById('lol')
-let newD = document.getElementById('newD')
-console.log('newD',newD)
-hi.style.color = 'blue'
-hi.style.backgroundColor = 'red'
-console.log(hi.style.color)
